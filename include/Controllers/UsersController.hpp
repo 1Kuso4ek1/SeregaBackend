a7c65444ed599c53e7542be116a0b63aa0543dc8
1Kuso4ek1 <dimasalov2006@gmail.com>
2025-08-28 21:00:52 +0300

    Implemented UsersController methods

--- include/Controllers/UsersController.hpp
+++ include/Controllers/UsersController.hpp
@@ -11,13 +11,15 @@ namespace Controllers
 class UsersController final : public HttpController<UsersController>
 {
 public:
-    static void getUser(const HttpRequestPtr& req, Callback&& callback, size_t id);
+    static void getUser(const HttpRequestPtr& req, Callback&& callback, int id);
+    static void getUserByUsername(const HttpRequestPtr& req, Callback&& callback, const std::string& username);
     static void getMe(const HttpRequestPtr& req, Callback&& callback);
 
 public:
     METHOD_LIST_BEGIN
 
-        ADD_METHOD_TO(getUser, "/users/{id}", Get);
+        ADD_METHOD_TO(getUser, "/users/{id}", Get, "Filters::JwtFilter");
+        ADD_METHOD_TO(getUserByUsername, "/users/name/{username}", Get, "Filters::JwtFilter");
         ADD_METHOD_TO(getMe, "/users/me", Get, "Filters::JwtFilter");
 
     METHOD_LIST_END
--- src/Controllers/UsersController.cpp
+++ src/Controllers/UsersController.cpp
@@ -1,5 +1,6 @@
 #include "Controllers/UsersController.hpp"
 
+#include "Models/UserKeys.hpp"
 #include "Models/Users.hpp"
 
 namespace Models = drogon_model::serega;
@@ -7,12 +8,51 @@ namespace Models = drogon_model::serega;
 namespace Controllers
 {
 
-void UsersController::getUser(const HttpRequestPtr& req, Callback&& callback, const size_t id)
+void UsersController::getUser(const HttpRequestPtr& req, Callback&& callback, const int id)
 {
+    static auto users = orm::Mapper<Models::Users>(app().getDbClient());
+    static auto keys = orm::Mapper<Models::UserKeys>(app().getDbClient());
+
+    try
+    {
+        const auto user = users.findOne({ "id", id });
+        const auto userKeys = user.getKeys(app().getDbClient());
+
+        if(userKeys.empty())
+            throw std::runtime_error("User has no keys");
+
+        Json::Value json;
+        json["id"] = *user.getId();
+        json["username"] = *user.getUsername();
+        json["identity_key"] = utils::base64Encode(userKeys.front().getIdentityKey()->data());
+        json["pre_key"] = utils::base64Encode(userKeys.front().getPreKey()->data());
+
+        callback(HttpResponse::newHttpJsonResponse(json));
+    }
+    catch(...)
+    {
+        callback(HttpResponse::newHttpResponse(k404NotFound, {}));
+    }
+}
+
+void UsersController::getUserByUsername(const HttpRequestPtr& req, Callback&& callback, const std::string& username)
+{
+    static auto users = orm::Mapper<Models::Users>(app().getDbClient());
+
+    try
+    {
+        const auto user = users.findOne({ "username", username });
+        getUser(req, std::move(callback), *user.getId());
+    }
+    catch(...)
+    {
+        callback(HttpResponse::newHttpResponse(k404NotFound, {}));
+    }
 }
 
 void UsersController::getMe(const HttpRequestPtr& req, Callback&& callback)
 {
+    getUser(req, std::move(callback), std::stoull(req->getParameter("user_id")));
 }
 
 }
